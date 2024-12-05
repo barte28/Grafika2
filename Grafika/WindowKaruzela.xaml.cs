85b9e3fc14a1edfddb6b145ab46ccef322e708fd
barte28 <bartek.teuerle@gmail.com>
2024-12-05 08:19:49 +0100

    petla zdjec

--- Grafika/WindowKaruzela.xaml.cs
+++ Grafika/WindowKaruzela.xaml.cs
@@ -19,9 +19,40 @@ namespace Grafika
     /// </summary>
     public partial class WindowKaruzela : Window
     {
+        public int Licznik { get; set; }
+        public List<string> obrazki = new List<string>();
         public WindowKaruzela()
         {
             InitializeComponent();
+            obrazki.Add("cat.jpg");
+            obrazki.Add("elephant.jpg");
+            obrazki.Add("greylag-goose.jpg");
+            obrazki.Add("mountains.jpg");
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Licznik--;
+            if(Licznik < 0)
+            {
+                Licznik = obrazki.Count - 1;
+            }
+            wyswietlObraz(Licznik);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Licznik++;
+            if(Licznik == obrazki.Count)
+            {
+                Licznik = 0;
+            }
+            wyswietlObraz(Licznik);
+        }
+        private void wyswietlObraz(int i)
+        {
+            image_modyfikowany.Source = new BitmapImage(new Uri(obrazki[i], UriKind.Relative));
         }
     }
 }
